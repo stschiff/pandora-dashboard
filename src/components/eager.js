43e886a1b26c8c725010fbdf6897d0118ce53b15
Stephan Schiffels <stephan_schiffels@mac.com>
2024-06-24 17:21:58 +0200

    added some first code to parse eager tables

--- src/components/eager.js
+++ src/components/eager.js
@@ -1,3 +1,39 @@
+async function loadEagerTable() {
+    const eager = await FileAttachment("./data/eager.tsv").zip();
+    const eagerTables = await Promise.all(eager.filenames.map(async fn => {
+        const batchName = fn.split("/")[6];
+        const tab = await eager.file(fn).tsv();
+        return tab.map(row => ({
+            batch_name: batch_name,
+            sample: row.Sample,
+            total_reads:  row["Samtools Flagstat (pre-samtools filter)_mqc-generalstats-samtools_flagstat_pre_samtools_filter-flagstat_total"],
+            mapped_reads: row["Samtools Flagstat (pre-samtools filter)_mqc-generalstats-samtools_flagstat_pre_samtools_filter-mapped_passed"],
+            mapped_reads_post: row["Samtools Flagstat (post-samtools filter)_mqc-generalstats-samtools_flagstat_post_samtools_filter-mapped_passed"],
+            endog_endorspy: row["endorSpy_mqc-generalstats-endorspy-endogenous_dna"],
+            endog_endorspy_post: row["endorSpy_mqc-generalstats-endorspy-endogenous_dna_post"],
+            mean_coverage: row["QualiMap_mqc-generalstats-qualimap-mean_coverage"],
+            damage_5p1: row["DamageProfiler_mqc-generalstats-damageprofiler-5_Prime1"],
+            damage_5p2: row["DamageProfiler_mqc-generalstats-damageprofiler-5_Prime2"],
+            damage_3p1: row["DamageProfiler_mqc-generalstats-damageprofiler-3_Prime1"],
+            damage_3p2: row["DamageProfiler_mqc-generalstats-damageprofiler-3_Prime2"],
+            mean_read_length: row["DamageProfiler_mqc-generalstats-damageprofiler-mean_readlength"],
+            nr_snps_covered: row["snp_coverage_mqc-generalstats-snp_coverage-Covered_Snps"],
+            contam: row["nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_estimate"],
+            contam_err: row["nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_SE"],
+            fastqc_preTrim_total_seq: row["FastQC (pre-Trimming)_mqc-generalstats-fastqc_pre_trimming-total_sequences"],
+            fastqc_postTrim_total_seq: row["FastQC (post-Trimming)_mqc-generalstats-fastqc_post_trimming-total_sequences"],
+            percent_duplicates: row["Picard_mqc-generalstats-picard-PERCENT_DUPLICATION"],
+            xrate: row["SexDetErrmine_mqc-generalstats-sexdeterrmine-RateX"],
+            yrate: row["SexDetErrmine_mqc-generalstats-sexdeterrmine-RateY"],
+            xrateErr: row["SexDetErrmine_mqc-generalstats-sexdeterrmine-RateErrX"],
+            yrateErr: row["SexDetErrmine_mqc-generalstats-sexdeterrmine-RateErrY"],
+            endog_manual: row.mapped_reads / row.total_reads * 100.0,
+            endo_manual_post: row.mapped_reads_post / total_reads * 100.0
+        }));
+    }));
+    const eager_table = eager_tables.reduce((acc, curr) => acc.concat(curr), []);
+}
+
 Sample,
 eager_batch,
 total_reads = `Samtools Flagstat (pre-samtools filter)_mqc-generalstats-samtools_flagstat_pre_samtools_filter-flagstat_total`,
