2c123884a7f6f60579d3efc67e2c8d7f3835ffaf
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-08-30 16:22:20 +0200

    works with async API

--- src/data/test_db.duckdb.js
+++ src/data/test_db.duckdb.js
@@ -1,95 +1,40 @@
 import { tableToIPC, tableFromJSON } from 'apache-arrow';
-import duckdb from 'duckdb'
-import fs from 'fs/promises';
-
-function duckDbQuery(dbCon, q) {
-    return new Promise((resolve, reject) => {
-      dbCon.all(q, function(err, res) {
-        if(err) return reject(err);
-        resolve(res);
-      });
-    })
-  }
-  
-  function duckDbBuffer(dbCon, name, arrowTable) {
-    return new Promise((resolve, reject) => {
-      dbCon.register_buffer(name, [tableToIPC(arrowTable)], true, (err, res) => {
-        if (err) return reject(err)
-        resolve(res);
-      });
-    })
-  }
-  
+import { Database } from "duckdb-async";
+import fs from 'fs/promises';  
 
 const dbFile = "/tmp/test_db.duckdb";
 fs.rm(dbFile, {force: true});
-const db = new duckdb.Database(dbFile);
-const con = db.connect();
+const db = await Database.create(dbFile);
+const con = await db.connect();
 
-// await duckDbQuery(con, `CREATE TABLE Persons (
-//     PersonID int,
-//     LastName varchar(255),
-//     FirstName varchar(255),
-//     Address varchar(255),
-//     City varchar(255)
-// )`);
-
-// await duckDbQuery(con, `INSERT INTO Persons VALUES (1, 'Stephan', 'Schiffels', 'AugBeb16', 'Leipzig')`);
-// await duckDbQuery(con, `INSERT INTO Persons VALUES (2, 'Xaver', 'Schiffels', 'ErzBAC', 'Aachen')`);
-
-con.exec(`CREATE TABLE Persons (
+await con.exec(`CREATE TABLE Persons (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255)
-    )`, (err) => {
-        if(err) console.warn(err);
-        con.exec(`
-            INSERT INTO Persons VALUES (1, 'Stephan', 'Schiffels', 'AugBeb16', 'Leipzig');
-            INSERT INTO Persons VALUES (2, 'Xaver', 'Schiffels', 'ErzBAC', 'Aachen');
-            `, async (err) => {
-                if(err) console.warn(err);
-                const fh = await fs.open(dbFile);
-                const readStream = fh.createReadStream();
-                readStream.pipe(process.stdout);
-            }
-        );
-    }
-);
+)`);
 
+await con.exec(`INSERT INTO Persons VALUES (1, 'Stephan', 'Schiffels', 'AugBeb16', 'Leipzig')`);
+await con.exec(`INSERT INTO Persons VALUES (2, 'Xaver', 'Schiffels', 'ErzBAC', 'Aachen')`);
 
-// const res = await duckDbQuery(con, `SELECT * FROM Persons`);
+// const res = await con.all("SHOW ALL TABLES");
+// const res = await con.all("SELECT * FROM Persons");
 // console.log(res);
-// await duckDbQuery(con, "SHOW ALL TABLES")
-// con.all("SELECT * FROM Persons", function(err, res) {console.log(res)});
-// con.all("SHOW TABLES", (err, res) => console.log(res));
 
-// const jsonData = [
-//   {"userId":1,"id":1,"title":"delectus aut autem","completed":false},
-//   {"userId":1,"id":2,"title":"quis ut nam facilis et officia qui","completed":false}
-// ];
+const jsonData = [
+  {"userId":1,"id":1,"title":"delectus aut autem","completed":false},
+  {"userId":1,"id":2,"title":"quis ut nam facilis et officia qui","completed":false}
+];  
+await con.exec(`INSTALL arrow; LOAD arrow`);
+const arrowTable = tableFromJSON(jsonData);
+await con.register_buffer("jsonDataTable", [tableToIPC(arrowTable)], true);
+await con.exec("CREATE TABLE testTable AS SELECT * FROM jsonDataTable");
 
-// const jsonData2 = [
-//     {"userId":2,"id":1,"title":"delectus aut autem","completed":false},
-//     {"userId":2,"id":2,"title":"quis ut nam facilis et officia qui","completed":false}
-//   ];
-  
-// con.exec(`INSTALL arrow; LOAD arrow;`, (err) => {
-//     if (err) {
-//         console.warn(err);
-//         return;
-//     }
-
-//     const arrowTable = tableFromJSON(jsonData.concat(jsonData2));
-//     con.register_buffer("jsonDataTable", [tableToIPC(arrowTable)], true, (err) => {
-//         if (err) {
-//             console.warn(err); 
-//             return;
-//         }
-//     con.exec("CREATE TABLE testTable AS SELECT * FROM jsonDataTable;");
-//     });
-// });
 
 // output database file to stdout, so that Framework can cache its result.
-
+await con.close();
+await db.close();
+const fh = await fs.open(dbFile);
+const readStream = fh.createReadStream();
+readStream.pipe(process.stdout);
--- src/playground.md
+++ src/playground.md
@@ -6,6 +6,15 @@ const db = await DuckDBClient.of({base: FileAttachment("./data/test_db.duckdb")}
 
 ```js
 const res = await db.query(`SHOW ALL TABLES`);
-// const res = await db.query("SHOW ALL TABLES");
+view(Inputs.table(res));
+```
+
+```js
+const res = await db.query(`SELECT * FROM base.testTable`);
+view(Inputs.table(res));
+```
+
+```js
+const res = await db.query(`SELECT * FROM base.Persons`);
 view(Inputs.table(res));
 ```
